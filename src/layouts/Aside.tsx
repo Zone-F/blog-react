cf359da49b822c871df1f01005d76e7c8b5863cd
zx_zhangyongfa <yongfa_zhang@fzzixun.com>
2021-07-01 09:54:30 +0800

    'webpack:eject前提交'

---
+++ src/layouts/Aside.tsx
@@ -0,0 +1,13 @@
+// @flow 
+// import * as React from 'react';
+type Props = {
+
+};
+const Aside = (props: Props) => {
+  return (
+    <div>
+      <div>侧边导航栏</div>
+    </div>
+  );
+};
+export default Aside
